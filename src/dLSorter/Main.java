7d41ca848d12a1c7407740007bc0196c384ef4eb
Grey <48751572+GreyStinger@users.noreply.github.com>
2022-10-17 16:04:11 +0200

    Update Main.java
    Removed a comment, probably not important

--- src/dLSorter/Main.java
+++ src/dLSorter/Main.java
@@ -3,7 +3,7 @@ package dLSorter;
 public class Main {
 	public static void main(String[] args) {
 		ConfigManager configManager = new ConfigManager();
-//
+		
 		Sorter sorter = new Sorter(configManager.readDownloadDir());
 
 		if (configManager.readDownloadDir().isEmpty())
@@ -11,7 +11,5 @@ public class Main {
 		else {
 			sorter.scanSort();
 		}
-		
-//		Gui.start(configManager);
 	}
 }
